2b1876e0f5c444afdc8460f5fdd563e2e8642e4a
Paweł <cpawols@gmail.com>
2014-02-12 21:59:52 +0100

    Blanodljcpirjvdiox

--- test.cpp
+++ test.cpp
@@ -7,18 +7,32 @@
 using namespace std;
 
 
-int main()
+int main(int argc, char** argv)
 {
+
 	Plansza p;
- 	p.wczytaj();
-	p.wypisz();
-	while(1)
-	{	
-		p.rusz_milosza();
-		p.rusz_reszte();
+	bool gramy = true;
+
+	while(gramy)
+	{
+		p.wczytaj(argv);
 		p.wypisz();
 
+		while(!p.daj_koniec_gry())
+		{
+			p.rusz_milosza();
+			p.rusz_reszte();
+			p.wypisz();
+		}
+		char c;
+		cin>>c;
+		if(c=='N')
+		{
+			p.ustaw_koniec_gry(false);
+			gramy = true;
+		}
+		else
+			gramy = false;
 	}
-
 	return 0;
 }
